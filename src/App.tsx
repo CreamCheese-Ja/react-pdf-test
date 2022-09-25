a0403f43d2533a21e510a7480bbb7741687ba2f9
Chisato Shiraga <creamcheese.japan@gmail.com>
2022-09-26 00:47:53 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import BasicDocument from "./BasicDocument";
+import { BlobProvider, PDFDownloadLink } from "@react-pdf/renderer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PDFDownloadLink document={<BasicDocument />} fileName="test.pdf">
+        {({ blob, url, loading, error }) =>
+          loading ? "Loading document..." : "クリックでPDFをダウンロード"
+        }
+      </PDFDownloadLink>
+
+      <BlobProvider document={<BasicDocument />}>
+        {({ blob, url, loading, error }) => {
+          // Do whatever you need with blob here
+          return (
+            <p>
+              <a
+                href={url ? url : ""}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                別タブでPDFを表示
+              </a>
+            </p>
+          );
+        }}
+      </BlobProvider>
     </div>
   );
 }
